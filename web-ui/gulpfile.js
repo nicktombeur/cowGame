b4e87c8186cc25da421c49cb83ebe53cb6949aaf
Nick Tombeur <nicktombeur@gmail.com>
2015-04-20 20:33:30 +0200

    sass

--- web-ui/gulpfile.js
+++ web-ui/gulpfile.js
@@ -1,8 +1,23 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    sass = require('gulp-sass');
 
-gulp.task('webserver', function() {
-    connect.server();
+gulp.task('sass', function() {
+    gulp.src('./assets/css/**/*.sass')
+        .pipe(sass({
+            indentedSyntax: true
+        }))
+        .pipe(gulp.dest('./assets/css'));
 });
 
-gulp.task('default', ['webserver']);
+gulp.task('watch', function() {
+    gulp.watch('./assets/css/**/*.sass', ['sass']);
+});
+
+gulp.task('webserver', function() {
+    connect.server({
+        livereload: true
+    });
+});
+
+gulp.task('default', ['webserver', 'sass', 'watch']);
