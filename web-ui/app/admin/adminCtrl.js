9dc578350cf5bf20cab0bc8b1d931f28e83f47ec
Martinus1990 <maarten.denorme@gmail.com>
2015-05-31 11:25:38 +0200

    Added Admin-OverviewPage

--- web-ui/app/admin/adminCtrl.js
+++ web-ui/app/admin/adminCtrl.js
@@ -2,6 +2,9 @@ define(['angular', 'app.ctrl', 'jquery'], function (angular, ctrls, $) {
 
     ctrls.controller('AdminController', ['$scope', 'adminService', function ($scope,adminService) {
         var vm = this;
+
+        vm.games = adminService.getGames();
+
     }]);
 
 
--- web-ui/app/admin/adminServices.js
+++ web-ui/app/admin/adminServices.js
@@ -15,7 +15,20 @@ define(['app.service'], function (services) {
                         throw httpError.status + " : " +
                         httpError.data;
                     });
+            },
+            getGames: function(){
+                return [{id:"1234",title:"First Game",shortDescription: "First Game description",fullDescription:"ezufohezuofzeufhezufohezufhezufheuzifheziufhezuifhezf"},
+                        {id:"123453",title:"First Game",shortDescription: "First Game description",fullDescription:"ezufohezuofzeufhezufohezufhezufheuzifheziufhezuifhezf"},
+                        {id:"123453",title:"First Game",shortDescription: "First Game description",fullDescription:"ezufohezuofzeufhezufohezufhezufheuzifheziufhezuifhezf"},
+                        {id:"123453",title:"First Game",shortDescription: "First Game description",fullDescription:"ezufohezuofzeufhezufohezufhezufheuzifheziufhezuifhezf"},
+                        {id:"123453",title:"First Game",shortDescription: "First Game description",fullDescription:"ezufohezuofzeufhezufohezufhezufheuzifheziufhezuifhezf"},
+                        {id:"123453",title:"First Game",shortDescription: "First Game description",fullDescription:"ezufohezuofzeufhezufohezufhezufheuzifheziufhezuifhezf"},
+                        {id:"123453",title:"First Game",shortDescription: "First Game description",fullDescription:"ezufohezuofzeufhezufohezufhezufheuzifheziufhezuifhezf"},
+                        {id:"123453",title:"First Game",shortDescription: "First Game description",fullDescription:"ezufohezuofzeufhezufohezufhezufheuzifheziufhezuifhezf"},
+                        {id:"123453",title:"First Game",shortDescription: "First Game description",fullDescription:"ezufohezuofzeufhezufohezufhezufheuzifheziufhezuifhezf"},
+                        {id:"12343434",title:"First Game",shortDescription: "First Game description",fullDescription:"Dieper afzien aan sterke are gezift herten gif. Kongostaat verwachten middellijn gas ongebruikt feestdagen buitendien mee. Koopman genomen javanen ik nu ontdekt op menigte. Bakken europa tunnel invoer zullen dat kamper zoo. Leveren zwijnen hoogere duivels sombere bak van dik lot. Uit stam hier aan acre zin. Schuld forten zeggen elk bij minste jungle leggen een. Dragen aan bouwde lappen loopen rubben ceylon nog. De afkoopen ad vreemden er af scheppen gewonnen. "}]
             }
+
         }
     }]);
 });
